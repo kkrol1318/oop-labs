63b422a56f536dfd4eeb95b2e6cdb9e0aaf8568d
Kamila Krol <kkrol.1325@wp.pl>
2025-11-12 13:34:06 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,26 @@
+﻿namespace Simulator
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        public Creature()
+        {
+            // bezparametrowy konstruktor - nic nie robi
+        }
+
+        public Creature(string name, int level = 1)
+        {
+            Name = name;
+            Level = level;
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi! I'm {Name} at level {Level}.");
+        }
+
+        public string Info => $"{Name} <{Level}>";
+    }
+}
---
+++ Simulator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Simulator!\n");
+            Creature shrek = new("Shrek", 5);
+            shrek.SayHi();
+            Console.WriteLine(shrek.Info);
+
+            Animals dogs = new() { Description = "Dogs" };
+            Console.WriteLine(dogs.Info);
+
+        }
+    }
+}
